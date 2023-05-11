ef59e0a1c0dad98e6b8150a10098ecfa489948f5
Philippe Casgrain <github@casgrain.com>
2023-05-11 19:42:44 -0400

    Initial Commit

---
+++ PumpItSpy/PumpItSpyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PumpItSpyApp.swift
+//  PumpItSpy
+//
+//  Created by Philippe on 2023-05-11.
+//
+
+import SwiftUI
+
+@main
+struct PumpItSpyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
